adb94d2ea2db4b36b48ed610fcae0df31d596f0a
Gaurav Ghildiyal <gauravkg@google.com>
2023-07-30 23:39:03 -0700

    Add initial set of CEL validations for HTTPRoute

--- apis/v1beta1/object_reference_types.go
+++ apis/v1beta1/object_reference_types.go
@@ -91,6 +91,8 @@ type SecretObjectReference struct {
 // References to objects with invalid Group and Kind are not valid, and must
 // be rejected by the implementation, with appropriate Conditions set
 // on the containing object.
+//
+// +kubebuilder:validation:XValidation:message="Must have port for Service reference",rule="((!has(self.group) || size(self.group) == 0) && (!has(self.kind) || self.kind == 'Service')) ? has(self.port) : true"
 type BackendObjectReference struct {
 	// Group is the group of the referent. For example, "gateway.networking.k8s.io".
 	// When unspecified or empty string, core API group is inferred.
